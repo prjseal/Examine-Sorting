58ef1aa5f1c08ed5666addf1ccdd94c0cf4e5d7e
Paul Seal <prjseal@gmail.com>
2024-08-15 12:16:40 +0100

    got it working thanks to Callum Whyte

---
+++ MyProject/Search/ConfigureIndexOptions.cs
@@ -0,0 +1,28 @@
+﻿using Examine;
+using Examine.Lucene;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Infrastructure.Examine;
+
+namespace MyProject.Search
+{
+    public class ConfigureIndexOptions : IConfigureNamedOptions<LuceneDirectoryIndexOptions>
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ConfigureIndexOptions(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Configure(string name, LuceneDirectoryIndexOptions options)
+        {
+            var nodeNameSortableFieldDefinition = new FieldDefinition(
+                UmbracoExamineFieldNames.NodeNameFieldName, // nodeName
+                FieldDefinitionTypes.FullTextSortable);
+
+            options.FieldDefinitions.AddOrUpdate(nodeNameSortableFieldDefinition);
+        }
+
+        public void Configure(LuceneDirectoryIndexOptions options) => Configure(string.Empty, options);
+    }
+}
